9d6b0e1e5f627f75e14f36517d497c883937078c
Alex <alkutin@gmail.com>
2019-06-26 01:39:34 +0300

    black theme

---
+++ MasterSlaveSite/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/app/app.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $.signalR.masterSlaveHub.on('slaveCodeRegistered', function (data) {
+        console.log('slaveCodeRegistered', data);
+    });
+
+    $.signalR.hub.start(function () {
+        $.signalR.masterSlaveHub.server.registerSlave();
+    });
+});
+
+var slaveCommands = {
+    OpenThisUrl: function (slave, url, mode) {
+        $.signalR.masterSlaveHub.server.openThisUrl(slave, url, mode);
+    }
+};
